6752943d716f5c783b122b2b0458ab2910d0b95f
CorrieRB <corriebelanger@gmail.com>
2022-01-27 12:33:27 -0800

    Added blast db update script

---
+++ update_databases.R
@@ -0,0 +1,19 @@
+#Setting up any required packages------------------------------------------
+
+writeLines('PATH="${RTOOLS40_HOME}/usr/bin;${PATH}"', con = "~/.Renviron")
+devtools::install_github("mhahsler/rBLAST")
+
+#downloading/updating databases--------------------------------------------
+
+download.file("ftp://ftp.ncbi.nlm.nih.gov/blast/db/16S_ribosomal_RNA.tar.gz",
+              "C:\\Users\\corri\\Sequencing_Analysis\\NCBI\\blast-2.12.0+\\db\\16S_ribosomal_RNA.tar.gz", mode='wb')
+untar("../NCBI/blast-2.12.0+/db/16S_ribosomal_RNA.tar.gz", exdir="../NCBI/blast-2.12.0+/db/16S_ribosomal_RNA")
+
+download.file("ftp://ftp.ncbi.nlm.nih.gov/blast/db/ITS_RefSeq_Fungi.tar.gz",
+              "C:\\Users\\corri\\Sequencing_Analysis\\NCBI\\blast-2.12.0+\\db\\ITS_RefSeq_Fungi.tar.gz", mode='wb')
+untar("../NCBI/blast-2.12.0+/db/ITS_RefSeq_Fungi.tar.gz", exdir="../NCBI/blast-2.12.0+/db/ITS_RefSeq_Fungi")
+
+
+download.file("ftp://ftp.ncbi.nlm.nih.gov/blast/db/taxdb.tar.gz",
+              "../NCBI/blast-2.12.0+/taxdb.tar.gz", mode='wb')
+untar("../NCBI/blast-2.12.0+/taxdb.tar.gz", exdir="../NCBI/blast-2.12.0+/taxdb")
